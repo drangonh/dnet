1d96eac1fa1745fbb9638088e46b0d12f9af3ada
hl <786676631@qq.com>
2020-03-10 16:58:28 +0800

    创建项目

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {post, get} from "./client/FetchData";
-import TestComponent from './page/Test'
 
-// Provider是react-redux两个核心工具之一，作用：将store传递到每个项目中的组件中
-// 第二个工具是connect，稍后会作介绍
-import {Provider} from 'react-redux'
-// 引入创建好的store实例
-import store from './redux/store'
-
-// 渲染DOM
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-                {/* 将store作为prop传入，即可使应用中的所有组件使用store */}
-                <Provider store={store}>
-                    <TestComponent/>
-                </Provider>
-            </div>
-        )
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
